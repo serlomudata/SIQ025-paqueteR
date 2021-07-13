db2d08e4f8e5abf3d92499a721801e918ec8dafb
Sergio <serlomu.data@gmail.com>
2021-07-13 12:10:42 +0000

    Commit inicial

---
+++ R/resta.R
@@ -0,0 +1,17 @@
+#' Resta entre dos numeros
+#' 
+#' Esta funcion devuelve la resta entre dos numeros.
+#'
+#' @param a Primer numero
+#' @param b Segundo numero
+#'
+#' @return La resta entre a y b
+#' @export
+#'
+#' @examples
+#' resta(5.2,6)
+#' 
+#' @seealso \link{suma}
+resta<- function(a,b){
+  return(a-b)
+}
---
+++ R/suma.R
@@ -0,0 +1,17 @@
+#' Suma de dos numeros
+#' 
+#' Esta funcion devuelve la suma entre dos numeros.
+#'
+#' @param a Primer numero
+#' @param b Segundo numero
+#'
+#' @return La suma entre a y b
+#' @export 
+#'
+#' @examples
+#' suma(5,3)
+#' 
+#' @seealso \link{resta}
+suma<- function(a,b){
+  return(a+b)
+}
